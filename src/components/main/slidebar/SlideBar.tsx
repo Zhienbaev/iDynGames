f84ea3a0e356706938f4228e4b2b0acd160313ca
Zhienbaev <Aidynzhienbaev@gmail.com>
2025-06-26 22:59:00 +0500

    fix v2 sidebar

--- src/components/main/slidebar/SlideBar.tsx
+++ src/components/main/slidebar/SlideBar.tsx
@@ -21,7 +21,7 @@ export function SlideBar() {
         if (pathname === '/') {
           setPage('main')
         } else {
-          setPage('balance')
+          setPage('bank')
         } 
       }, [pathname])
 
