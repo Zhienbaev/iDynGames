3806684bf97255658f162c9c0854907ac9c4e029
Zhienbaev <Aidynzhienbaev@gmail.com>
2025-06-26 21:34:59 +0500

    authentication

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,12 @@
+// middleware.ts
 import { getToken } from "next-auth/jwt"
 import { NextResponse } from "next/server"
 import type { NextRequest } from "next/server"
 
 export async function middleware(req: NextRequest) {
   const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET })
+
+  console.log("TOKEN IN MIDDLEWARE:", token)
 
   const isAuth = !!token
   const isAuthPage = req.nextUrl.pathname.startsWith("/auth")
@@ -19,9 +22,6 @@ export async function middleware(req: NextRequest) {
   return NextResponse.next()
 }
 
-
 export const config = {
-  matcher: [
-    "/((?!_next/static|_next/image|favicon.ico|api/auth|favicon.ico).*)",
-  ],
+  matcher: ["/((?!_next/static|_next/image|favicon.ico|api/auth).*)"],
 }
--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -1,10 +1,14 @@
-'use client'
+
 
 import Header from "@/app/(home)/(main)/Header";
 import { SlideBar } from "@/components/main/slidebar/SlideBar";
 import { Stack } from "@mui/material";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/lib/authOptions";
+import { redirect } from "next/navigation";
+
 
 // Подключаем Inter
 const inter = Inter({
@@ -15,11 +19,16 @@ const inter = Inter({
 
 
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const session = await getServerSession(authOptions);
+
+  if (!session) {
+    redirect("/auth/login");
+  }
   return (
     <Stack direction={"row"}>
       <SlideBar />
